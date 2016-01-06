207e2d7618a32867233af27d712e3af72410e828
Daniel Klemm <daniel.klemm@datameer.com>
2016-01-06 16:27:49 +0100

    frame manager update

--- src/backend/FrameManager.java
+++ src/backend/FrameManager.java
@@ -11,8 +11,11 @@ import java.awt.event.WindowFocusListener;
 
 import javax.swing.JFrame;
 
+import database.entities.Kunde;
 import datameer.com.google.common.base.Optional;
 import frontend.EigenverbrauchFrame;
+import frontend.KundeDataFrame;
+import frontend.KundeDeleteFrame;
 import frontend.KundenFrame;
 import frontend.MainFrame;
 import frontend.Notification;
@@ -29,8 +32,11 @@ public class FrameManager {
     public static final ActionListener openKunden(final JFrame frame) {
 	return new ActionListener() {
 	    public void actionPerformed(ActionEvent e) {
-		KundenFrame kundenFrame = new KundenFrame();
-		kundenFrame.setVisible(true);
+		if (!MainFrame._notificationKeeper._notification.isPresent()) {
+		    KundenFrame kundenFrame = new KundenFrame();
+		    kundenFrame.setVisible(true);
+		    MainFrame._frameKeeper._openFrame = Optional.<TypedJFrame> of(kundenFrame);
+		}
 	    }
 	};
     }
@@ -38,8 +44,11 @@ public class FrameManager {
     public static final ActionListener openEigenverbrauch(final JFrame frame) {
 	return new ActionListener() {
 	    public void actionPerformed(ActionEvent e) {
-		EigenverbrauchFrame eigenverbrauchFrame = new EigenverbrauchFrame();
-		eigenverbrauchFrame.setVisible(true);
+		if (!MainFrame._notificationKeeper._notification.isPresent()) {
+		    EigenverbrauchFrame eigenverbrauchFrame = new EigenverbrauchFrame();
+		    eigenverbrauchFrame.setVisible(true);
+		    MainFrame._frameKeeper._openFrame = Optional.<TypedJFrame> of(eigenverbrauchFrame);
+		}
 	    }
 	};
     }
@@ -52,20 +61,12 @@ public class FrameManager {
 	};
     }
 
-    public static final MouseAdapter closeFrame(final JFrame frame) {
-	return new MouseAdapter() {
-	    @Override
-	    public void mouseClicked(MouseEvent e) {
-		frame.dispose();
-	    }
-	};
-    }
-
     public static final ActionListener closeNotificationByButton(final JFrame frame) {
 	return new ActionListener() {
 	    @Override
 	    public void actionPerformed(ActionEvent e) {
 		frame.dispose();
+		MainFrame._notificationKeeper._notification = Optional.absent();
 	    }
 	};
     }
@@ -84,14 +85,16 @@ public class FrameManager {
 	};
     }
 
+    public static final void showNotification(final boolean error, final String message1, final String message2) {
 	Notification notification = new Notification(error, message1, message2);
 	notification.setVisible(true);
+	MainFrame._notificationKeeper._notification = Optional.<TypedJFrame> of(notification);
     }
 
     public static final KeyAdapter numbersOnly() {
 	return new KeyAdapter() {
 	    private boolean dot = false;
-	    
+
 	    public void keyTyped(KeyEvent e) {
 		char caracter = e.getKeyChar();
 		if (dot) {
@@ -108,4 +111,51 @@ public class FrameManager {
 	};
     }
 
+    public static final MouseAdapter closeFrame(final JFrame frame) {
+	return new MouseAdapter() {
+	    @Override
+	    public void mouseClicked(MouseEvent e) {
+		frame.dispose();
+	    }
+	};
+    }
+
+    public static final WindowFocusListener checkOtherOpenFrames() {
+	return new WindowFocusListener() {
+	    public void windowGainedFocus(WindowEvent e) {
+		System.err.println("MainFrame window gained focus");
+		if (MainFrame._notificationKeeper._notification.isPresent()) {
+		    System.err.println("Notification window request focus");
+		    MainFrame._notificationKeeper._notification.get().requestFocus();
+		}
+		if (MainFrame._frameKeeper._openFrame.isPresent()) {
+		    System.err.println("Open window closed");
+		    MainFrame._frameKeeper._openFrame.get().dispose();
+		    MainFrame._frameKeeper._openFrame = Optional.<TypedJFrame> absent();
+		}
+	    }
+
+	    public void windowLostFocus(WindowEvent e) {
+		System.err.println("MainFrame window lost focus");
+	    }
+	};
+    }
+
+    public static final void showKundeDeleteCheck(Kunde kunde) {
+	KundeDeleteFrame kundeDeleteFrame = new KundeDeleteFrame(kunde);
+	kundeDeleteFrame.setVisible(true);
+	MainFrame._notificationKeeper._notification = Optional.<TypedJFrame> of(kundeDeleteFrame);
+    }
+
+    public static final void showKundeDataFrame(final Optional<Kunde> kunde, final KundenFrame kundenFrame) {
+	if (!MainFrame._notificationKeeper._notification.isPresent()) {
+	    if (MainFrame._frameKeeper._openFrame.isPresent()) {
+		MainFrame._frameKeeper._openFrame = Optional.absent();
+	    }
+	    KundeDataFrame kundeDataFrame = new KundeDataFrame(kunde);
+	    kundeDataFrame.setVisible(true);
+	    kundenFrame.dispose();
+	    MainFrame._frameKeeper._openFrame = Optional.<TypedJFrame> of(kundeDataFrame);
+	}
+    }
 }
